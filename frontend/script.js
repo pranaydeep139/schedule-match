bf40346be4135691c12c65859a5e8e82bfbfe34b
pranaydeep139 <pranaydeep139@gmail.com>
2025-08-03 01:45:51 +0530

    Seriously again? Another slash?

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://schedule-match-production.up.railway.app/"; // Backend URL
+const API_URL = "https://schedule-match-production.up.railway.app"; // Backend URL
 // --- Global State ---
 let currentUser = null;
 let currentWeekStart = null;
